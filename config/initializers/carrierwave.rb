01cd4ef135555dc82e908e1aad35d8c0df0d52f2
naoshimasamura <naoshi1112@icloud.com>
2020-08-07 21:45:19 +0900

    set deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,28 +4,6 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
 
-  case Rails.env
-  when 'production'
-   config.fog_credentials = {
-   provider:                'AWS',
-   aws_access_key_id: "your-aws-id",
-   aws_secret_access_key: "your-secret-key",
-   region:                  'your-region'
-  }
-    config.fog_directory = 'your-bucket-name'
-    config.asset_host = 'your-bucket-url'
-
-  when 'staging'
-   config.fog_credentials = {
-   provider:                'AWS',
-   aws_access_key_id:       "your-secret-id",
-   aws_secret_access_key:   "your-secret-key",
-   region:                  'your-region'
- }
-    config.fog_directory = 'your-secret-bucket-name'
-    config.asset_host = 'your-buucket-url'
-end
-end
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
